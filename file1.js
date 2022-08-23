087c7be4fd5842c2defd2f02e2c4d3cf336cbc50
Irfan khan <zackmarts2738@gmail.com>
2022-08-23 13:56:55 +0500

    15th commit in main branch

--- file1.js
+++ file1.js
@@ -10,9 +10,7 @@ console.log("What happened on epstein island???")
 
 console.log("What happened to epstein ???")
 
-console.log("What happened on epstein island???")
 
-console.log("What happened to epstein ???")
 
 
 
